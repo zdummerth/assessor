882d34cc5905f1818a8308635bfe17e6673b94b4
unknown <zach>
2024-11-04 12:32:23 -0600

    fix typescript error

--- components/ui/filters-sales.tsx
+++ components/ui/filters-sales.tsx
@@ -6,9 +6,9 @@ import {
 } from "./filter-client";
 import { Suspense } from "react";
 
-export default async function SalesFilters({ count }: { count: number }) {
+export default async function SalesFilters({ count }: { count?: number }) {
   return (
-    <div className="flex gap-4 relative z-10">
+    <div className="flex gap-4 relative z-10 overflow-x-auto whitespace-nowrap">
       <div className="">
         <ClientDisclosureWrapper
           serverComponent={
@@ -25,9 +25,11 @@ export default async function SalesFilters({ count }: { count: number }) {
       <div>
         <DateRangeMenu />
       </div>
-      <div className="absolute top-0 right-0">
-        <div className="p-2 rounded-md">{count} Sales</div>
-      </div>
+      {count && (
+        <div className="fixed top-0 right-10">
+          <div className="p-2 rounded-md bg-background">{count} Sales</div>
+        </div>
+      )}
     </div>
   );
 }
