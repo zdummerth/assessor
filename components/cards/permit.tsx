3e25e27fb4b555f61f8098d6740d502d319a3195
Zach Dummerth <zdummerth@gmail.com>
2025-02-07 14:52:40 -0600

    add permit card

--- components/cards/permit.tsx
+++ components/cards/permit.tsx
@@ -2,19 +2,6 @@
 import React, { useState } from "react";
 import { ArrowUpCircle, ArrowDownCircle } from "lucide-react";
 
-// {
-//       id: 162476,
-//       parcel_number: '3910-9-125.000',
-//       year: 2021,
-//       permit_type: 'BUILDING',
-//       status: 'ACTIVE',
-//       completion_date: '2021-12-06',
-//       issued_date: '2021-08-03',
-//       request: 'REPLACE EXISTING DECK (REAR) PER PLANS',
-//       cost: 9000,
-//       report_date: '2024-12-06'
-//     }
-
 const PermitCard: React.FC<{ permit: any }> = ({ permit }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
@@ -24,7 +11,7 @@ const PermitCard: React.FC<{ permit: any }> = ({ permit }) => {
 
   return (
     <div className="bg-background shadow-lg rounded-lg border border-gray-200 p-2">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-evenly">
         <p className="text-md font-bold">{permit.year}</p>
         <p className="text-md">{permit.status}</p>
         <p className="font-semibold">${permit.cost.toLocaleString()}</p>
