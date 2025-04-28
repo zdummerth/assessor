64184ce99b843ef69117306849e787840483b975
Zach Dummerth <zdummerth@gmail.com>
2025-04-28 16:36:08 -0500

    adjust address layout

--- components/ui/notices/fifteen/revised.tsx
+++ components/ui/notices/fifteen/revised.tsx
@@ -43,8 +43,8 @@ export default function Notice({ data }: { data: any }) {
       </div>
       <div className="w-full">
         <div id="detail" className="w-full">
-          <div className="flex justify-between mt-10 mb-16">
-            <div className="relative left-16">
+          <div className="flex justify-between mt-8 mb-16">
+            <div className="relative left-12">
               <p>{owner_name}</p>
               <p>{address_1}</p>
               {address_2 && <p>{address_2}</p>}
