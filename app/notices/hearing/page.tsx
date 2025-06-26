a10aaaa6dcac87a264f28106093fce10065ab3cb
Zach Dummerth <zdummerth@gmail.com>
2025-06-26 14:50:36 -0500

    remove props from hearing notice page

--- app/notices/hearing/page.tsx
+++ app/notices/hearing/page.tsx
@@ -1,9 +1,5 @@
 import Notice from "@/components/ui/notices/hearings/custom";
 
-export default async function Data({ listId }: { listId: number }) {
-  return (
-    <div className="">
-      <Notice />;
-    </div>
-  );
+export default function Page() {
+  return <Notice />;
 }
